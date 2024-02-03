9d41423939f16762f51f2c1005408b420f41160d
BiswajitNathaSharma <122850529+BiswajitNathaSharma@users.noreply.github.com>
2024-02-03 13:19:44 +0530

    random numbers complete

---
+++ access-random.js
@@ -0,0 +1,11 @@
+function extractElements(array) {
+    let [first, second, ...rest] = array;
+    
+    let last = rest.length > 0 ? rest[rest.length - 1] : undefined;
+
+    return [first, second, last];
+}
+
+let inputArray = [1, 2, 3, 4, 5];
+let resultArray = extractElements(inputArray);
+console.log(resultArray); 
